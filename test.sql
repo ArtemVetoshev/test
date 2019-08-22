5a039428699043639e351046220ec4a8b1f5314c
Artem Vetoshev <artem_vetoshev@epam.com>
2019-08-22 13:18:08 +0300

    Added group by and aggr function

--- test.sql
+++ test.sql
@@ -1,3 +1,4 @@
-select id, airline_name, status from airlines 
+select id, airline_name, status, count(planes) from airlines 
 where airline_name  like 'C%' and airline_name like 'D%'
+group by id, airline_name, status
 order by 2 desc;
